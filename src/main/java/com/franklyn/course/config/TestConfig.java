0589029f0fbab5270a47b8c2f6c972342015e3b2
Franklyn <franklyn1998@live.com>
2021-12-07 16:30:10 -0300

    Entidade Pedido. Datas com Instant e padrão ISO 8601 - PARTE 1

--- src/main/java/com/franklyn/course/config/TestConfig.java
+++ src/main/java/com/franklyn/course/config/TestConfig.java
@@ -14,10 +14,6 @@ import com.franklyn.course.repositories.UserRepository;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 
-//	Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-//	Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-//	Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-
 	@Autowired
 	private UserRepository userRepository;
 
