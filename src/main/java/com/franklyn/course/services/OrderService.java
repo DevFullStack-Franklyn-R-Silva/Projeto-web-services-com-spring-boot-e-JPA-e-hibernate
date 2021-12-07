88cc6cfdc31aead4bf04f9cf9c322f36ce42d844
Franklyn <franklyn1998@live.com>
2021-12-07 16:57:01 -0300

    Entidade Pedido. Datas com Instant e padrão ISO 8601 - PARTE 2

---
+++ src/main/java/com/franklyn/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.franklyn.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.franklyn.course.entities.Order;
+import com.franklyn.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	public List<Order> findAll() {
+		return orderRepository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = orderRepository.findById(id);
+		return obj.get();
+	}
+}
