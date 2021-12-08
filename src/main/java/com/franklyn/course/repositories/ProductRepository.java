d01a15f2205a10dd21fe31a57e1bbbc7c44eed20
Franklyn <franklyn1998@live.com>
2021-12-08 12:06:57 -0300

    Entidade Product

---
+++ src/main/java/com/franklyn/course/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.franklyn.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.franklyn.course.entities.Product;
+
+@Repository 
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
