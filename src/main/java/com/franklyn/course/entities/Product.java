5d30d22d34a8ab5abb1f3fad95368b2155898697
Franklyn <franklyn1998@live.com>
2021-12-08 15:15:24 -0300

    Entidade Payment, associação um para um

--- src/main/java/com/franklyn/course/entities/Product.java
+++ src/main/java/com/franklyn/course/entities/Product.java
@@ -19,7 +19,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
