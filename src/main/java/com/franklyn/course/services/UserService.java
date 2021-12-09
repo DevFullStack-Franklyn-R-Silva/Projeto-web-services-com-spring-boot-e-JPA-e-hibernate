76d2283b550a13efa10d333f947f435c984eacd2
Franklyn <franklyn1998@live.com>
2021-12-08 21:28:37 -0300

    Atualização de User

--- src/main/java/com/franklyn/course/services/UserService.java
+++ src/main/java/com/franklyn/course/services/UserService.java
@@ -27,8 +27,20 @@ public class UserService {
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		userRepository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = userRepository.getOne(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getEmail());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
