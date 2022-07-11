4df4eab559406f0f889c83951375cfe78b34943d
Franklyn Roberto <58437929+hadesfranklyn@users.noreply.github.com>
2022-07-11 19:04:35 -0300

    Atualizando o que faltava

--- src/main/java/com/franklyn/course/services/CategoryService.java
+++ src/main/java/com/franklyn/course/services/CategoryService.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.franklyn.course.entities.Category;
-import com.franklyn.course.entities.Order;
 import com.franklyn.course.repositories.CategoryRepository;
-import com.franklyn.course.repositories.OrderRepository;
 
 @Service
 public class CategoryService {
